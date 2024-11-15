b9b1d1c67cfce67ca5ec3cf776261215bf8b212e
devhabeeblateef <habeeblateef002@gmail.com>
2024-11-15 23:44:54 +0100

    header, create auth, passkeys, search page

---
+++ client/app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+       <>
+       <p>Hello World 123!</p>
+       <Button>Click Me</Button>
+       </>
+  );
+}
---
+++ client/app/(store)/search/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+async function SearchPage({
+    searchParams
+}: {
+    searchParams: {
+        query: string
+    };
+}) {
+    const {query} = await searchParams;
+
+  return <div>
+        SearchPage for {query}
+  </div>
+}
+
+export default SearchPage
